27a7edb3c3c4f17ba35b043a0f69297ee39ebc69
Bana Malik <bandanakm@gmail.com>
2019-03-09 17:35:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -18,27 +18,27 @@ end
   update_array_at_with(board, index, character)
  end
 
-
-the display board method
-  takes in a board array
-  creates a board displaying each index value of the array
-
-the input to index method
-  takes input
-  ensures that it is an integer
-  subtracts 1 to get the index value
-
-the update_array_at_with method
-  takes in a array, index, and value
-  updates the array index at a certain value
-
-the move method
-  takes in a board, index, and character
-  updates the board array, with the specific index, setting it to the char
-
-the cli
-  welcomes users
-  tells them to enter a value 1 to 9
-  sets input = the value they enter
-  sets the index equar to what they enter
-  calls the move method, with the board, index, char
+#
+# the display board method
+#   takes in a board array
+#   creates a board displaying each index value of the array
+#
+# the input to index method
+#   takes input
+#   ensures that it is an integer
+#   subtracts 1 to get the index value
+#
+# the update_array_at_with method
+#   takes in a array, index, and value
+#   updates the array index at a certain value
+#
+# the move method
+#   takes in a board, index, and character
+#   updates the board array, with the specific index, setting it to the char
+#
+# the cli
+#   welcomes users
+#   tells them to enter a value 1 to 9
+#   sets input = the value they enter
+#   sets the index equar to what they enter
+#   calls the move method, with the board, index, char
